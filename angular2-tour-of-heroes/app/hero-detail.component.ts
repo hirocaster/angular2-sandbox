a064dabb85a6afbf6556858fbf8c0ad18c958642
hirocaster <hohtsuka@gmail.com>
2016-03-04 01:23:44 +0900

    Fix errors

--- angular2-tour-of-heroes/app/hero-detail.component.ts
+++ angular2-tour-of-heroes/app/hero-detail.component.ts
@@ -6,28 +6,28 @@ import { HeroService } from './hero.service';
 
 
 @Component({
-    selector: 'my-hero-detail',
-    templateUrl: 'app/hero-detail.component.html'
-    styleUrls: ['app/hero-detail.component.css']
-    inputs: ['hero']
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css'],
+  inputs: ['hero']
 })
 
 export class HeroDetailComponent {
-    hero: Hero;
+  hero: Hero;
 
-    constructor(
-      private _heroService: HeroService,
-      private _routeParams: RouteParams) {
-    }
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
 
-    ngOnInit() {
-        let id = +this._routeParams.get('id');
-        this._heroService.getHero(id)
-            .then(hero => this.hero = hero);
-    }
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id)
+        .then(hero => this.hero = hero);
+  }
 
-    goBack() {
-        window.history.back();
-    }
+  goBack() {
+    window.history.back();
+  }
 
 }
